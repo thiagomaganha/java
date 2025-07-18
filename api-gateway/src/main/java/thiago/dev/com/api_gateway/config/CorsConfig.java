e52b8ab66222c30e0955caaed7a9239da179e19b
Thiago Maganha <t.soares9@gmail.com>
2025-07-18 12:01:13 -0300

    Creation of Api Gateway using Spring Cloud Gateway

---
+++ api-gateway/src/main/java/thiago/dev/com/api_gateway/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package thiago.dev.com.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("#{'${client-origin-url}'.split(',')}")
+    private List<String> clients;
+
+//    @Bean
+//    public CorsWebFilter corsConfiguration() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.addAllowedHeader("Authorization");
+//        corsConfiguration.addAllowedHeader("Content-Type");
+//        corsConfiguration.addAllowedHeader("Accept");
+//        corsConfiguration.addAllowedMethod("GET");
+//        corsConfiguration.addAllowedMethod("POST");
+//        corsConfiguration.addAllowedMethod("PUT");
+//        corsConfiguration.addAllowedMethod("DELETE");
+//        corsConfiguration.addAllowedMethod("OPTIONS");
+//        corsConfiguration.setAllowedOrigins(this.clients);
+//        corsConfiguration.setMaxAge(3600L);
+//        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//        return new CorsWebFilter(corsConfigurationSource);
+//    }
+}
